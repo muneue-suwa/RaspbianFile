ed6db037c0b15aed8c405bea4a2ac18aab4d7fd7
Dongsik <dongsiku+git@gmail.com>
2019-03-25 22:22:52 +0900

    Fix: sed in wrapup_raspbianfile.sh

--- wrapup_raspbianfile.sh
+++ wrapup_raspbianfile.sh
@@ -1,6 +1,6 @@
 # Comment Out Default Setting
-sudo sed -e "s/PubkeyAuthentication/#Rfile# PubkeyAuthentication/g" /etc/ssh/sshd_config
-sudo sed -e "s/PasswordAuthentication/#Rfile# PasswordAuthentication/g" /etc/ssh/sshd_config
+sudo sed -i -e "s/PubkeyAuthentication/#Rfile# PubkeyAuthentication/g" /etc/ssh/sshd_config
+sudo sed -i -e "s/PasswordAuthentication/#Rfile# PasswordAuthentication/g" /etc/ssh/sshd_config
 # Set The New Setting
-sudo sed -e "/#Rfile# PubkeyAuthentication/\i PubkeyAuthentication yes" /etc/ssh/sshd_config
-sudo sed -e "/#Rfile# PasswordAuthentication/\i PasswordAuthentication no" /etc/ssh/sshd_config
+sudo sed -i -e "/^#Rfile# PubkeyAuthentication$/a PubkeyAuthentication yes" /etc/ssh/sshd_config
+sudo sed -i -e "/^#Rfile# PasswordAuthentication$/a PasswordAuthentication no" /etc/ssh/sshd_config
