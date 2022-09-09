dcbc178b6bb5b188fcfe9a22cad5ef176a5920ba
Muneue SUWA <m_suwa@s.okayama-u.ac.jp>
2022-09-09 23:00:45 +0900

    Ubuntu用に作り替えた

--- RaspbianFile.sh
+++ RaspbianFile.sh
@@ -1,58 +1,45 @@
 #!/usr/bin/env bash
 
-# Get INSTALL_SH_DIRNAME
-RASPBIANFILE_SH_FILENAME=`readlink -f $0`
-RASPBIANFILE_SH_DIRNAME=`dirname $RASPBIANFILE_SH_FILENAME`
+# Get CREATE_NEW_USER_SH_DIRNAME
+CREATE_NEW_USER_SH_FILENAME=`readlink -f $0`
+CREATE_NEW_USER_SH_DIRNAME=`dirname $CREATE_NEW_USER_SH_FILENAME`
 
-# Read setting file
-if [ -f $RASPBIANFILE_SH_DIRNAME/settings/RaspbianFile.cfg ]; then
-    . $RASPBIANFILE_SH_DIRNAME/settings/RaspbianFile.cfg
-else
-    echo "Create settings file: RaspbianFile.cfg"
+NEW_USER=$1
+PUBLICKEY_FILENAME=$2
+
+# Check the number of arguments
+if [ $# -ne 2 ]; then
+    echo "Usage: bash create_new_user.sh USERNAME PUBLIC_KEY_FILENAME" 1>&2
     exit 1
 fi
 
-# Check The Public Key
-if [ -f $RASPBIANFILE_SH_DIRNAME/$PUBLICKEY_FILENAME ]; then
-    echo "Public Key was checked"
+# Check the public key
+if [ -f $PUBLICKEY_FILENAME ]; then
+    echo "Public Key DOES exist!"
 else
-    echo "Create public key: $PUBLICKEY_FILENAME"
+    echo "Create public key: $PUBLICKEY_FILENAME" 1>&2
     exit 1
 fi
 
-# NewUser
-## Create NewUser
+# Create NewUser
 sudo adduser $NEW_USER
-## Add Groups to NewUser
-PI_GROUPS=$(groups)
-NEW_USER_GROUP=$(echo ${PI_GROUPS//$USER/} | tr " " ",")
-# NEW_USER_GROUP=$(echo $PI_GROUPS | tr " " ",")
-sudo usermod -G $NEW_USER_GROUP $NEW_USER
 
-# SSH key
-## Copy The Public Key
+# Get the current user's groups
+CURRENT_USERS_GROUPS=$(groups)
+
+# Add the new user to the current user's groups
+NEW_USER_GROUP=$(echo ${CURRENT_USERS_GROUPS//$USER/} | sed s/sudo\ // | tr " " ",")
+echo "$NEW_USER's secondary groups: $NEW_USER_GROUP"
+sudo usermod -aG $NEW_USER_GROUP $NEW_USER
+
+# Copy the public key
 sudo mkdir -p /home/$NEW_USER/.ssh
-sudo cp $RASPBIANFILE_SH_DIRNAME/$PUBLICKEY_FILENAME /home/$NEW_USER/.ssh/authorized_keys
-## Change owner, group, and permisson of Public Key
+sudo cp $CREATE_NEW_USER_SH_DIRNAME/$PUBLICKEY_FILENAME /home/$NEW_USER/.ssh/authorized_keys
+
+# Change owner, group, and permisson of Public Key
 sudo chown -R $NEW_USER:$NEW_USER /home/$NEW_USER/.ssh
 sudo chmod 700 /home/$NEW_USER/.ssh
 sudo chmod 600 /home/$NEW_USER/.ssh/authorized_keys
-
-# Set SSH Server
-## Make a Backup
-sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.org
-## Comment Out Default Setting
-sudo sed -i -e "s/PermitRootLogin/#ByRaspbianFile# PermitRootLogin/g" /etc/ssh/sshd_config
-sudo sed -i -e "s/PermitEmptyPasswords/#ByRaspbianFile# PermitEmptyPasswords/g" /etc/ssh/sshd_config
-## Set The New Setting
-sudo sed -i -e "/^#*ByRaspbianFile# PermitRootLogin .*$/a PermitRootLogin no" /etc/ssh/sshd_config
-sudo sed -i -e "/^#*ByRaspbianFile# PermitEmptyPasswords .*$/a PermitEmptyPasswords no" /etc/ssh/sshd_config
-## Copy wrapup_raspbianfile.sh to the home directory
-sudo cp $RASPBIANFILE_SH_DIRNAME/wrapup_raspbianfile.sh /home/$NEW_USER/
-sudo chown -R $NEW_USER:$NEW_USER /home/$NEW_USER/wrapup_raspbianfile.sh
-
-# Remove RaspbianFile from /home/pi/
-rm -rf $RASPBIANFILE_SH_DIRNAME
 
 # Completion message
 echo "Complited!"
--- wrapup_raspbianfile.sh
+++
@@ -1,6 +0,0 @@
-# Comment Out Default Setting
-sudo sed -i -e "s/PubkeyAuthentication/#ByRaspbianFile# PubkeyAuthentication/g" /etc/ssh/sshd_config
-sudo sed -i -e "s/PasswordAuthentication/#ByRaspbianFile# PasswordAuthentication/g" /etc/ssh/sshd_config
-# Set The New Setting
-sudo sed -i -e "/^#*ByRaspbianFile# PubkeyAuthentication .*$/a PubkeyAuthentication yes" /etc/ssh/sshd_config
-sudo sed -i -e "/^#*ByRaspbianFile# PasswordAuthentication .*$/a PasswordAuthentication no" /etc/ssh/sshd_config
